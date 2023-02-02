4b4733627fd9ede132a36ee9551b44053869e5d8
Nataleesha <frojlan@gmail.com>
2023-02-02 03:33:41 +0200

    2

---
+++ src/js/02-timer.js
@@ -0,0 +1,68 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const flatpickr = require('flatpickr');
+
+const inputEl = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() - this.config.defaultDate.getTime() < 0) {
+      Notify.failure('Please choose a date in the future');
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+const calendar = flatpickr(inputEl, options);
+
+startBtn.addEventListener('click', timeCount);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  value = value.toString();
+  if (value.length < 2) {
+    return value.padStart(2, '0');
+  }
+  return value;
+}
+
+function timeCount() {
+  Notify.success('Countdown has started!');
+  startBtn.removeEventListener('click', timeCount);
+  setInterval(() => {
+    const currentTime = Date.now();
+    const remainTime = calendar.selectedDates[0].getTime() - currentTime;
+    const mms = convertMs(remainTime);
+    daysEl.textContent = addLeadingZero(mms.days);
+    hoursEl.textContent = addLeadingZero(mms.hours);
+    minutesEl.textContent = addLeadingZero(mms.minutes);
+    secondsEl.textContent = addLeadingZero(mms.seconds);
+  }, 1000);
+}
